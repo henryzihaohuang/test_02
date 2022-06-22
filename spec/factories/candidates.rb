0c2eb963102a8841e299a71c14268ab98e656a97
Henry Huang <zihao@henrys-mbp.lan>
2022-06-22 08:46:36 -0700

    change number

--- spec/factories/candidates.rb
+++ spec/factories/candidates.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do 
   factory :candidate do 
     full_name { Faker::Name.name }
-    uid { Faker::Number.number(digits: 1000000) }
+    uid { Faker::Number.number(digits: 1000) }
   end
 end
