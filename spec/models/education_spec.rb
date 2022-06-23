e9e730116b0ffd85b0664304a84f7c05369cd2b0
Henry Huang <zihao@henrys-mbp.lan>
2022-06-23 12:20:54 -0700

    test

--- spec/models/education_spec.rb
+++ spec/models/education_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Education, type: :model do
   let(:education) { FactoryBot.create(:education, uid: 1)}
-  let(:education_without_dates) { create(:education, :without_dates)}
-  let(:has_graduated_education) { create(:education, :has_graduated) } 
-  let(:current_student_education) { create(:education, :current_student) }
+  let(:education_without_dates) { create(:education, start_year: nil, start_month: nil, end_year: nil, end_month: nil)}
+  let(:has_graduated_education) { create(:education, start_year: Date.current.year - 8, start_month: Date.current_month, end_year: Date.current.year - 4, end_month: Date.current.month + 1) } 
+  let(:current_student_education) { create(:education, start_year: Date.current.year - 1, start_month: Date.current_month, end_year: Date.current.year + 3, end_month: Date.current.month + 1) }
 
   describe 'associations' do 
     it { should belong_to(:candidate) }
